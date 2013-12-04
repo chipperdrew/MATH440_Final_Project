5b3779c66807563eb888df750722d369f0aed90e
savethetauntauns <quintero.lucas.e@gmail.com>
2013-12-04 04:10:03 +0000

    Collision Working with wall collisions too
    I pushed something!

--- serial.cpp
+++ serial.cpp
@@ -92,59 +92,97 @@ double moveDist(){
 	return pow(-1.0,rand() % 2) * (5.0*rand()/RAND_MAX);
 }
 
+
 void collision(Particle *particleList, int numlist){
-	double xval,yterm,xterm,yconst,xconst,plust,negt;
-	double m1,m2,t;
-	double x1,x2,y1,y2;
+	double m1,m2; //Slopes
+	double xval,yval;//absolute collision
+	double A,B,C,negt,plust; //For Quadratic Eqn
+	double yterm,yconst,xterm,xconst; //For simplifying *term is the variable coefficent
+	double t,x1,x2,y1,y2; //For location of collision
+	double tmin; //First collision
+
 	for(int i=0 ; i<numlist ; i++){
-		m1=(particleList[i].getnewY()-particleList[i].getoldY())/(particleList[i].getnewX()-particleList[i].getoldX());
-		for(int j=i+1 ; j<numlist-1 ; j++){
-			//xval is the value of x for the intersection of two lines
-			//this is found by (y2-y1)=m(x2-x1) for two sets of two points,
-			//solving for m of each system then setting y equal for the
-			//respective y=(y2-y1)/(x2-x1)*(x2-x1)+y1
-			m2=(particleList[j].getnewY()-particleList[j].getoldY())/(particleList[j].getnewX()-particleList[j].getoldX());
-			xval=(particleList[j].getoldY()-particleList[j].getoldX()*m1)-(particleList[i].getoldY()-particleList[i].getoldX()*m2);
-			xval=xval/(m1-m2);
-	  	if((0<(particleList[i].getoldX()-xval) && 0>(particleList[i].getnewX()-xval))||
-	  			(0>(particleList[i].getoldX()-xval) && 0<(particleList[i].getnewX()-xval))){
-	  		if((0<(particleList[j].getoldX()-xval) && 0>(particleList[j].getnewX()-xval))||
-	  			(0>(particleList[j].getoldX()-xval) && 0<(particleList[j].getnewX()-xval))){
-					//Thus a collision is possible, so lets find where each particle was at time t
-					//Thus there should exist 2 points where the two points are separated by the two radius
-					//so we look where (y2-y1)^2+(x2-x1)^2=(2R)^2
-					//after some analysis, we get (yterm*t+yconst)^2-(xterm*t+xconst)^2=(radius1+radius2)^2
+		tmin=10000000.000;
+//		//cout<<"ENTERED 1\n";
+		// m=(y2-y1)/(x2-x1)
+		//m2=(particleList[i].getnewY()-particleList[i].getoldY())/(particleList[i].getnewX()-particleList[i].getoldX());
+		for(int j=i+1 ; j<numlist ; j++){		
+//			//cout<<"ENTERED 2\n";
+			if(i==j){
+				continue;
+			}
+		
+			yterm=((particleList[j].getnewY()-particleList[j].getoldY())-(particleList[i].getnewY()-particleList[i].getoldY()));
+			yconst=(particleList[j].getoldY()-particleList[i].getoldY());
+			xterm=((particleList[j].getnewX()-particleList[j].getoldX())-(particleList[i].getnewX()-particleList[i].getoldX()));
+			xconst=particleList[j].getoldX()-particleList[i].getoldX();
 
-//WAKA WAKA LUCAS CHECK THE DAMN CODE, YOU MAY HAVE DRANK 2 TOO MANY
-//WHAT TO DO: FIND THE PROJECTION OF ONE PATH ON TO THE OTHER, THEN FIND THE INVERSE TANGENT, THEN BISECT, THEN REFLECT BY INTERIOR ANGLES, AND CONTINUE
-//ALSO, CONSIDER FINDING THE CLOSEST INTERSECTION (VALLUE WHERE X-XVAL EXISTS AND IS SMALL), THEN RETURNING THE J VALUE, THEN DOING THE MOVEMENT,
-//THEN DOING THE MOVEMENT AND THEN SETTNIG A BOOL SO THAT THE PARTICLE HAS TO CHECK THAT THERE ARE NO OTHER COLLISIONS, ONCE THERE ARE NUM_PARTS-1-J
-// "NO" COLLSIONS THEN GO TO THE NEXT PARTICLE (NOTE:P1 CHECKS 2->NUM-1, P2 CHECKS 3->NUM-1...)
-					yterm=(particleList[j].getnewY()-particleList[j].getoldY())-(particleList[i].getnewY()-particleList[i].getoldY());
-					yconst=(particleList[j].getoldX()/(particleList[j].getnewX()-particleList[j].getoldX()))-(particleList[i].getoldX()/(particleList[i].getnewX()-particleList[i].getoldX()))+particleList[j].getoldY()-particleList[i].getoldY();
-					xterm=(particleList[j].getnewX()-particleList[j].getoldX())-(particleList[i].getnewX()-particleList[i].getoldX());
-					xconst=(particleList[j].getoldX()-particleList[i].getoldX());
-					plust=-(2*(yconst*yterm+xterm*xconst))+sqrt(pow(2*(yconst*yterm+xterm*xconst),2)-4*(pow(yterm,2)+pow(xterm,2))*(pow(yconst,2)*pow(xconst,2)));
-					negt=-(2*(yconst*yterm+xterm*xconst))-sqrt(pow(2*(yconst*yterm+xterm*xconst),2)-4*(pow(yterm,2)+pow(xterm,2))*(pow(yconst,2)*pow(xconst,2)));
-					//We want t to be postive (as t is a time, so its trivially why)
-					//We want the shortest t of the two t's as that is the initial collsion
-					if(negt<0 && plust>0){
-						t=plust;
-					} else if(plust<0 && negt<0){
-						t=negt;
-					} else if(plust>0 && negt>0){
-						if(plust<negt){
-							t=plust;
-						}
-						else{
-							t=negt;
-						}
-						cout<<t<<endl;
-					//x1=t*x()
-					}
+			A=pow(yterm,2)+pow(xterm,2);
+			B=2*(yterm*yconst+xterm*xconst);
+			C=pow(yconst,2)+pow(xconst,2)-pow(0,2);
+			if((pow(B,2)-4*A*C)<0)
+				break;
+			plust=(-B+sqrt(pow(B,2)-4*A*C))/(2*A);
+			negt=(-B-sqrt(pow(B,2)-4*A*C))/(2*A);
+			//We want t to be postive (as t is a time, so its trivially why)
+			//We want the shortest t of the two t's as that is the initial collsion
+			if(negt<0 && plust>0 && plust<=1){
+				t=plust;
+			} else if(plust<0 && negt>0 && negt<=1){
+				t=negt;
+			} else if(plust>0 && negt>0 && (plust<=1 || negt<=1)){
+				if(plust<negt){
+					t=plust;
+				}
+				else{
+					t=negt;
+				}
+			} else{
+				break;
+			}
 
-	  		}
-	  	}
+			if(t<tmin){
+				tmin=t;
+			}
+		}//cout<<particleList[0]<<endl;
+		if(i!=numlist-1 && tmin<=1 && tmin>0){
+			//cout<<tmin<<endl;
+			particleList[i].setnewX(.95*tmin*(particleList[i].getnewX()-particleList[i].getoldX())+particleList[i].getoldX());
+			particleList[i].setnewY(.95*tmin*(particleList[i].getnewY()-particleList[i].getoldY())+particleList[i].getoldY());
+		}
+		//cout<<particleList[0]<<endl;
+		if(fabs(particleList[i].getnewX()) > CHAMBER_WIDTH/2){
+			// Check if collision occurs on pos. wall
+			//cout<<"hey"<<endl;
+			if(particleList[i].getoldX()>0){
+				//cout<<"move"<<endl;
+				// Check if escaped
+				double dist_from_wall = CHAMBER_WIDTH/2 -particleList[i].getoldX();
+				double time_collision = dist_from_wall / (particleList[i].getnewX()-particleList[i].getoldX());
+				double y_at_time = time_collision*(particleList[i].getnewX()-particleList[i].getoldX()) + particleList[i].getoldX();
+			//	if(fabs(y_at_time) < HALF_ESCAPE_WALL_WIDTH) {
+			//		setnewX(fromx+moveX); // X escapes
+			//	} else {
+					particleList[i].setnewX(CHAMBER_WIDTH-particleList[i].getnewX());
+			//	}
+			// Else collision on neg. wall (no escape possible)
+			} else {
+				particleList[i].setnewX(-CHAMBER_WIDTH-particleList[i].getnewX());
+			}
+		}
+		// Else no collision
+		else {	
+			particleList[i].setnewX(particleList[i].getnewX());
+		}
+		// Check for Y wall collisions
+		if(fabs(particleList[i].getnewY()) > CHAMBER_HEIGHT/2.0){
+			if(particleList[i].getoldY()>0){
+				particleList[i].setnewY(CHAMBER_HEIGHT-particleList[i].getnewY());
+			} else {
+				particleList[i].setnewY(-CHAMBER_HEIGHT-particleList[i].getnewY());
+			}	
+		} else {
+			particleList[i].setnewY(particleList[i].getnewY());
 		}
 	}
 }
